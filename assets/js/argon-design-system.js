5f9734af6aee0f637396285cf33e37ffc1bf9c28
Toma Rares <tomairares@gmail.com>
2020-07-03 11:18:54 +0300

    Refactor(scrollbar): Fix error

--- assets/js/argon-design-system.js
+++ assets/js/argon-design-system.js
@@ -37,7 +37,7 @@ var $tagsinput = $('.tagsinput');
   if (isWindows) {
     // if we are on windows OS we activate the perfectScrollbar function
 
-    $('.wrapper .login-page, .register-page, .card').perfectScrollbar();
+    var ps1 = new PerfectScrollbar('.wrapper .login-page, .register-page, .card');
 
 
     if ($('.tab-content .table-responsive').length != 0) {
